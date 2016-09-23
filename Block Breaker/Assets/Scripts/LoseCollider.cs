8185460c3482dfa31122d9bb4e2c8fd5b503e7b2
Fernando Rocha Silva <rocha@vpn-0-231.soho.slb.com>
2016-09-22 23:05:36 -0500

    Fixing sizes

---
+++ Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+	void OnTriggerEnter2D (Collider2D trigger){
+		print ("Trigger");	
+		levelManager.LoadLevel ("Lose Screen");
+	}
+
+	void OnCollisionEnter2D (Collision2D collision){
+		print ("Collision");	
+	}
+}
---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
+		var mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;	
+
+		print (mousePosInBlocks);
+
+		paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
+		this.transform.position = paddlePos;
+
+	}
+}
