f932f66e46f496fe2d9527e9686385a9799d5a8d
Fernando Rocha <fernandorsl@gmail.com>
2016-09-22 20:41:23 -0500

    Adding colliders

---
+++ Block Breaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	public static MusicPlayer instance = null;
+
+	void Awake (){
+		if (instance != null) {
+			Destroy (gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+		} 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
