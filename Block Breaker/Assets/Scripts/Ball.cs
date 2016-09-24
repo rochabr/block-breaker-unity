3f6d224c64030557d61fd7310d7e38467db743c7
Fernando Rocha Silva <rocha@Fernandos-MBP-3.attlocal.net>
2016-09-24 07:27:46 -0500

    Changing script execution order / changing resolution to 800 x 600

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	public Paddle paddle;
+
+	private bool hasStarted = false;
+	private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+		//set the ball position + the addle position to the variable
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasStarted) {
+			//Lock the ball relative to the paddle
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+
+			//Wait for the mouse press to launch
+			if (Input.GetMouseButtonDown(0)) {
+				hasStarted = true;
+				print ("Launch ball");
+				this.GetComponent<Rigidbody2D> ().velocity = new Vector2(2f, 10f);
+			}
+		}
+	}
+}
